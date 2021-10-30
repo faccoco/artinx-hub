b666c48e67f2b04ebca04110dd763ca2363b9aa7
Jincheng Yu <12012223@mail.sustech.edu.cn>
2021-10-30 22:54:53 +0800

    perf(color calibration):overall modification

--- src/Preprocess/ColorCalibration.cpp
+++ src/Preprocess/ColorCalibration.cpp
@@ -4,6 +4,7 @@
 #include "Hub.hpp"
 #include <caf/event_based_actor.hpp>
 #include <cstdint>
+#include <fmt/format.h>
 #include <opencv2/mcc.hpp>
 
 struct ColorCalibratorSettings final {};
@@ -16,27 +17,27 @@ bool inspect(Inspector& f, ColorCalibratorSettings& x) {
 class ColorCalibrator final : public HubHelper<caf::event_based_actor, ColorCalibratorSettings, image_frame_atom> {
     struct CalibrationData final {
         mutable cv::ccm::ColorCorrectionModel model;
+        cv::Ptr<cv::mcc::CCheckerDetector> detector = cv::mcc::CCheckerDetector::create();
     };
 
     Identifier mKey;
     std::optional<CalibrationData> mCalibratedData;
 
     void detectColorCheckerAndCalibrate(const cv::Mat& frame) {
-        cv::Mat detectedFrame;
-        cv::Ptr<cv::mcc::CCheckerDetector> detector = cv::mcc::CCheckerDetector::create();
-        if(!detector->process(detectedFrame, cv::mcc::MCC24, 1)) {
+        const auto& data = mCalibratedData.value();
+        if(!data.detector->process(frame, cv::mcc::MCC24, 1, true)) {
             CAF_LOG_INFO("ChartColor not detected \n");
             return;
         }
-        cv::Ptr<cv::mcc::CChecker> checker = detector->getBestColorChecker();
-        cv::Mat chartsRGB = checker->getChartsRGB();
-        cv::Mat src = chartsRGB.col(1).clone().reshape(3, chartsRGB.rows / 3);
+        cv::Ptr<cv::mcc::CChecker> checker = data.detector->getBestColorChecker();
+        const auto chartsRGB = checker->getChartsRGB();
+        const auto src = chartsRGB.col(1).clone().reshape(3, chartsRGB.rows / 3);
         src /= 255.0;
         cv::ccm::ColorCorrectionModel model(src, cv::ccm::COLORCHECKER_Macbeth);
         model.run();
         const auto loss = model.getLoss();
         if(loss >= 8) {
-            CAF_LOG_INFO("loss: " << loss);
+            CAF_LOG_INFO(fmt::format("loss:{:.2f}", loss));
             return;
         }
         mCalibratedData->model = model;
@@ -45,14 +46,14 @@ class ColorCalibrator final : public HubHelper<caf::event_based_actor, ColorCali
     cv::Mat applyCalibration(const cv::Mat& frame) const {
         const auto& data = mCalibratedData.value();
         cv::Mat image;
-        cvtColor(frame, image, cv::COLOR_BGR2RGB);
+        cv::cvtColor(frame, image, cv::COLOR_BGR2RGB);
         image.convertTo(image, CV_64F);
         image /= 255;
-        cv::Mat calibratedImage = data.model.infer(image);
+        const auto calibratedImage = data.model.infer(image);
         cv::Mat out = calibratedImage * 255;
         out.convertTo(out, CV_8UC3);
         cv::Mat outImage;
-        cv::cvtColor(min(max(out, 0), 255), outImage, cv::COLOR_RGB2BGR);
+        cv::cvtColor(cv::min(cv::max(out, 0), 255), outImage, cv::COLOR_RGB2BGR);
         return outImage;
     }
 
