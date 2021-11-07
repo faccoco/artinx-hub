73f23c05923e29c1c563d5e9462839db16ad03f7
Haoxuan Qin <12111321@mail.sustech.edu.cn>
2021-11-07 15:22:52 +0800

    Anglesolver

--- src/Solve/AngleSolver.cpp
+++ src/Solve/AngleSolver.cpp
@@ -9,11 +9,13 @@
 #include <cstdint>
 #include <cmath>
 #include <fmt/format.h>
-struct AngleSolverSettings final {};
+struct AngleSolverSettings final {
+    double precision;
+};
 
 template <class Inspector>
 bool inspect(Inspector& f, AngleSolverSettings& x) {
-    return f.object(x).fields();
+    return f.object(x).fields(f.field("precision",x.precision));
 }
 
 class AngleSolver final : public HubHelper<caf::event_based_actor, AngleSolverSettings, shoot_atom, set_target_posture_atom> {
@@ -30,8 +32,8 @@ public:
                      const auto dataPosture = BlackBoard::instance().get<PostureData>(mIMUKey);
                      const auto globalSettings = BlackBoard::instance().get<GlobalSettings>({}).value();
                      const double g = -globalSettings.gForce, bulletSpeed = 15.00;
-                     auto square = [=](double x) {return x * x};
-                     auto cube = [=](double x) {return x*x*x}；
+                     auto square = [=](double x) { return x * x; };
+                     auto cube = [=](double x) { return x * x * x; };
                      
                      Vector<UnitType::Distance, FrameOfReference::Gun> positionOfReferenceGun (data.value().center.value().raw());
                      glm::dvec3 transformedPosition = { 0, 0, 0};
@@ -50,16 +52,16 @@ public:
                          double yawAngle = std::atan2(transformedPosition.y, transformedPosition.x) - glm::half_pi<double>();
                          if(yawAngle < 0)
                              yawAngle += glm::two_pi<double>();
-                         double expr1 = std::sqrt(square(bulletSpeed)* square(bulletSpeed) - 2 * g * transformedPosition.z * square(bulletSpeed) - square(horizonalDistance * g, )) / square(g);
+                         const auto expr1 = std::sqrt(square(bulletSpeed)* square(bulletSpeed) - 2 * g * transformedPosition.z * square(bulletSpeed) - square(horizonalDistance * g) ) / square(g);
 
-                         double expr2 = std::sqrt(square(bulletSpeed / g) - transformedPosition.z / g - expr1);
-                         double expr3 = 2 * std::sqrt(2) * horizonalDistance * ((-2 * g * transformedPosition.z + 2 * std::pow(bulletSpeed, 2)) * expr2 - square(g) * cube(expr2));
+                         const auto expr2 = std::sqrt(square(bulletSpeed / g) - transformedPosition.z / g - expr1);
+                         auto expr3 = 2 * glm::root_two<double>() * horizonalDistance * ((-2 * g * transformedPosition.z + 2 * std::pow(bulletSpeed, 2)) * expr2 - square(g) * cube(expr2));
 
                          expr3 /= (double)(4 * bulletSpeed);
                          expr3 /= (square(transformedPosition.x) + square(transformedPosition.y) +
                                    square(transformedPosition.z));
 
-                         double expr4 = 2 * std::sqrt(2) *
+                         auto expr4 = 2 * glm::root_two<double>() *
                              (-square(horizonalDistance) * g * expr2 + square(transformedPosition.z) * g * expr2 -
                               2 * transformedPosition.z * square(bulletSpeed) * expr2 +
                               square(g) * transformedPosition.z * cube(expr2));
@@ -69,14 +71,16 @@ public:
 								square(transformedPosition.z));
                          double pitchAngle = std::atan2(expr4, expr3);
                          pitchAngle = (pitchAngle < glm::quarter_pi<double>() / 4) ? pitchAngle : (glm::half_pi<double>() / 2 - pitchAngle);
-                         CAF_LOG_INFO(fmt::format("YawAngle: {},PitchAngle: {}", yawAngle, pitchAngle));
+                         //CAF_LOG_INFO(fmt::format("YawAngle: {},PitchAngle: {}", yawAngle, pitchAngle));
                          double currentYawAngle = std::atan2(forwardVector.y, forwardVector.x);
                          double currentPitchAngle = std::atan2(forwardVector.z, std::hypot(forwardVector.x, forwardVector.y));
-                         CAF_LOG_INFO(fmt::format("CurrentYawAngle: {},CurrentPitchAngle: {}",currentYawAngle, currentPitchAngle));
-                         double precision = 0.01;
-                         bool ifShoot = ( (std::abs(currentPitchAngle - pitchAngle) < precision) && ( (std::abs(currentYawAngle - yawAngle) < precision) || (std::abs(currentYawAngle - glm::half_pi<double>() - yawAngle) < precision) ) );
+                         //CAF_LOG_INFO(fmt::format("CurrentYawAngle: {},CurrentPitchAngle: {}",currentYawAngle, currentPitchAngle));
+                         //double prec = mConfig.precision;
+                         double prec = 0.001;
+                         CAF_LOG_INFO(fmt::format("Prec:{}", prec));
+                         bool ifShoot = ( (std::abs(currentPitchAngle - pitchAngle) < prec) && ( (std::abs(currentYawAngle - yawAngle) < prec) || (std::abs(currentYawAngle - glm::half_pi<double>() - yawAngle) < prec) ) );
                          sendAll(set_target_posture_atom_v, yawAngle, pitchAngle);
-                         sendAll(shoot_atom_v, true);
+                         sendAll(shoot_atom_v, ifShoot);
                      }
                      
                       
