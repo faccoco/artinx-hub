84c25581e180e1af0714da51d9119761a5d0050e
Yingwei Zheng <dtcxzyw@qq.com>
2022-06-23 10:45:30 +0800

    ci(deploy): fix deploy config

--- scripts/setup-service.bash
+++ scripts/setup-service.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo systemctl stop ArtinxHub.service
-sudo kill $(pidof ArtinxHub)
-sudo rm -rf /opt/artinx-hub
+sudo systemctl stop ArtinxHub.service >/dev/null
+sudo kill $(pidof ArtinxHub) >/dev/null
+sudo rm -rf /opt/artinx-hub >/dev/null
 sudo cp -r ./ /opt/artinx-hub
 sudo cp -f ./bringup_templates/ArtinxHub.service /lib/systemd/system/ArtinxHub.service
 sudo echo $1 > /opt/deploy_target.conf
