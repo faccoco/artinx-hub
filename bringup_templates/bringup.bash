27589929b14fd272ad8ee5f35c5ff4d9f222382e
AtlasGooo <1774525013@qq.com>
2022-06-23 11:18:02 +0800

    ci(deploy): fix deploy config

--- bringup_templates/bringup.bash
+++ bringup_templates/bringup.bash
@@ -11,9 +11,3 @@ while [ true ]; do
   /opt/artinx-hub/build/src/ArtinxHub /opt/artinx-hub/deploy_config/$(cat /opt/deploy_target.conf).conf > /opt/artinx-hub.log
   sleep 1
 done
-# Guidance:
-#  To use this sh script, add the following command to Ubuntu Application Startup:
-#   gnome-terminal -- bash [absolute file path to this script]  //deprecated, doesn't work on two nuc.
-#   gnome-terminal -e "bash [absolute file path to this script]" // tested to work on two nuc.
-#  for example,
-#   gnome-terminal -e "bash /home/artinx-004/workspace/codes/artinx-hub/bringup_templates/bringup.bash"
--- scripts/setup-service.bash
+++ scripts/setup-service.bash
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 sudo systemctl stop ArtinxHub.service >/dev/null
-sudo kill $(pidof ArtinxHub) >/dev/null
-sudo rm -rf /opt/artinx-hub >/dev/null
+sudo kill $(pidof ArtinxHub) 2>/dev/null
+sudo rm -rf /opt/artinx-hub
 sudo cp -r ./ /opt/artinx-hub
 sudo cp -f ./bringup_templates/ArtinxHub.service /lib/systemd/system/ArtinxHub.service
 sudo echo $1 > /opt/deploy_target.conf
 sudo systemctl enable ArtinxHub.service
-sleep 5
-sudo systemctl status ArtinxHub.service
