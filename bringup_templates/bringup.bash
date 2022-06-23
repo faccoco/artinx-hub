4d64307c6a8a051bd4ac44de2bb2e34b23ece20b
Yingwei Zheng <dtcxzyw@qq.com>
2022-06-23 10:02:47 +0800

    ci(deploy): fix deploy config

--- bringup_templates/bringup.bash
+++ bringup_templates/bringup.bash
@@ -8,7 +8,7 @@ cd "$path" || exit # if fails to cd, exits.
 cd ..
 #ArtinxHub
 while [ true ]; do
-  /opt/artinx-hub/build/src/ArtinxHub /opt/artinx-hub/deploy_config/$DEPLOY_TARGET.conf
+  /opt/artinx-hub/build/src/ArtinxHub /opt/artinx-hub/deploy_config/$(cat /opt/deploy_target.conf).conf > /opt/artinx-hub.log
   sleep 1
 done
 # Guidance:
--- scripts/setup-service.bash
+++ scripts/setup-service.bash
@@ -5,6 +5,7 @@ kill $(pidof ArtinxHub)
 rm -rf /opt/artinx-hub
 cp -r ./ /opt/artinx-hub
 cp -f ./bringup_templates/ArtinxHub.service /lib/systemd/system/ArtinxHub.service
+echo $1 > /opt/deploy_target.conf
 systemctl enable ArtinxHub.service
 sleep 5
 systemctl status ArtinxHub.service
