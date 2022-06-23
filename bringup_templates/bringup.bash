3bdd11adb103894f9021f59e1cbd72414de88d6c
Yingwei Zheng <dtcxzyw@qq.com>
2022-06-23 13:43:52 +0800

    ci(deploy): fix usb2 camera

--- bringup_templates/bringup.bash
+++ bringup_templates/bringup.bash
@@ -1,6 +1,8 @@
 # openvino
 sleep 3
 source /opt/intel/openvino_2021/bin/setupvars.sh
+source /opt/env_setup.sh
+
 # cd to the bash script location
 str=$0 # the bash script location
 path=$(dirname "$str") # the directory of the location
@@ -8,6 +10,6 @@ cd "$path" || exit # if fails to cd, exits.
 cd ..
 #ArtinxHub
 while [ true ]; do
-  /opt/artinx-hub/build/src/ArtinxHub /opt/artinx-hub/deploy_config/$(cat /opt/deploy_target.conf).conf > /opt/artinx-hub.log
+  sudo /opt/artinx-hub/build/src/ArtinxHub /opt/artinx-hub/deploy_config/$(cat /opt/deploy_target.conf).conf > /opt/artinx-hub.log
   sleep 1
 done
