72c571395e4c42247e02e5fbbb14835b49b7fc50
zhihuiLiang <1518705978@qq.com>
2022-10-09 20:28:52 +0800

    feat(ArmorPredictor): finish predictor coding

--- src/Postprocess/ArmorPredictor.cpp
+++ src/Postprocess/ArmorPredictor.cpp
@@ -1,4 +1,20 @@
-#include "ArmorPredictor.hpp"
+#include "BlackBoard.hpp"
+#include "DataDesc.hpp"
+#include "ExceptionProbe.hpp"
+#include "HeadInfo.hpp"
+#include "Hub.hpp"
+#include "PostureData.hpp"
+#include "SelectedTarget.hpp"
+#include "Utility.hpp"
+#include <cstdint>
+#include <utility>
+
+#include "SuppressWarningBegin.hpp"
+
+#include <caf/event_based_actor.hpp>
+#include <eigen3/Eigen/Dense>
+#include <fmt/format.h>
+#include <magic_enum.hpp>
 
 constexpr double maxDeltaTime = 0.2;
 constexpr int32_t maxCntWrongData = 5;
@@ -11,122 +27,195 @@ constexpr double maxXVel = 3.0;
 constexpr double maxYVel = 3.0;
 constexpr double maxZVel = 1.0;
 
-glm::dvec3 KalmanFilter::getPredictedVel(){
-    return mPredictedVel;
+struct ArmorPredictorSettings final {
+    bool enablePredictor;
+};
+
+template <class Inspector>
+bool inspect(Inspector& f, ArmorPredictorSettings& x) {
+    return f.object(x).fields(f.field("enalePredictor", x.enablePredictor));
 }
 
-void KalmanFilter::initialKalmanFilter(const glm::dvec3& measuredPos, const TimePoint& curTimePoint) {
-    mX.resize(6);
-    mF.setIdentity(6, 6);
-    mP.setIdentity(6, 6);
-    mQ.setIdentity(6, 6);
-    mH.resize(3, 6);
-    mH << 1.0, 0.0, 0.0,  //
-        0.0, 1.0, 0.0,    //
-        0.0, 0.0, 1.0;    //
-    mR.resize(3, 3);
-    mR << 0.01, 0.0, 0.0,  //
-        0.0, 0.01, 0.0,    //
-        0.0, 0.0, 0.01;
+class ArmorPredictor final
+    : public HubHelper<caf::event_based_actor, ArmorPredictorSettings, armor_detect_available_atom, image_frame_atom> {
+    Identifier mKey, mIMUKey, mHeadKey;
+    GroupMask mGroupMask;
 
-    mLastPos = measuredPos;
-    mLastTimePoint = curTimePoint;
-    setX(measuredPos);
-    mInitFlag = true;
-}
+    bool mInitFlag = false;
+    int32_t mCntWrongData = 0;
+    glm::dvec3 mLastPos;
+    TimePoint mLastTimePoint;
+    glm::dvec3 mPredictedVel;
+    Eigen::VectorXd mX;  // State vector(Position & Velocity)
+    Eigen::MatrixXd mF;  // State transform mat
+    Eigen::MatrixXd mP;  // State covariance mat
+    Eigen::MatrixXd mQ;  // Process covariance mat
+    Eigen::MatrixXd mH;  // Measurement mat
+    Eigen::MatrixXd mR;  // Measurement covariance mat
 
-void KalmanFilter::Prediction() {
-    mX = mF * mX;
-    mP = mF * mP * mF.transpose() + mQ;
-}
-
-void KalmanFilter::UpdateMeasurement(const Eigen::VectorXd z) {
-    const auto y = z - mH * mX;  // Measure
-    const auto S = mH * mP * mH.transpose() + mR;
-    const auto K = mP * mH.transpose() * S.inverse();  // Kalman Gain
-    mX = mX + (K * y);                                 // Optimal estimate
-    const auto I = Eigen::MatrixXd::Identity(mX.size(), mX.size());
-    mP = (I - K * mH) * mP;
-}
-
-void KalmanFilter::KmFilter(const glm::dvec3& pos, double dt) {
-
-    Eigen::MatrixXd inputF(6, 6);
-    inputF << 1.0, 0.0, 0.0, dt, 0.0, 0.0,  //
-        0.0, 1.0, 0.0, 0.0, dt, 0.0,        //
-        0.0, 0.0, 1.0, 0.0, 0.0, dt,        //
-        0.0, 0.0, 0.0, 1.0, 0.0, 0.0,       //
-        0.0, 0.0, 0.0, 0.0, 1.0, 0.0,       //
-        0.0, 0.0, 0.0, 0.0, 0.0, 1.0;       //
-    mF << inputF;
-
-    Prediction();
-    Eigen::VectorXd measuredZ(3, 1);
-    measuredZ << pos.x, pos.y, pos.z;
-    UpdateMeasurement(measuredZ);
-
-    mPredictedVel = { mX(3), mX(4), mX(5) };
-    if (mPredictedVel.x > maxXVel){
-        mPredictedVel.x = maxXVel;
-    }
-    if (mPredictedVel.y > maxYVel){
-        mPredictedVel.y = maxYVel;
-    }
-    if (mPredictedVel.z > maxZVel){
-        mPredictedVel.z = maxZVel;
-    }
-}
-
-void KalmanFilter::RunFilter(const glm::dvec3& measuredPos, const TimePoint& curTimePoint) {
-    if (!mInitFlag){
-        initialKalmanFilter(measuredPos, curTimePoint);
-        return;
+    void setX(const glm::dvec3& measuredPos) {
+        Eigen::VectorXd initX(6, 1);
+        initX << measuredPos.x, measuredPos.y, measuredPos.z, 0.0, 0.0, 0.0;
+        mX = initX;
     }
 
-    double deltaTime = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(curTimePoint - mLastTimePoint).count()) / 1e6;
-    if(deltaTime > maxDeltaTime) {      
-        initialKalmanFilter(measuredPos, curTimePoint);
-        return;
-    }
-    const auto filterPos = filterWrongData(measuredPos);
-    if (!mInitFlag){
-        initialKalmanFilter(measuredPos, curTimePoint);
-        return;
-    }
-    KmFilter(filterPos, deltaTime);
-}
-
-void KalmanFilter::setX(const glm::dvec3& measuredPos){
-    Eigen::VectorXd initX(6, 1);
-    initX << measuredPos.x, measuredPos.y, measuredPos.z, 0.0, 0.0, 0.0;
-}
-
-glm::dvec3 KalmanFilter::filterWrongData(const glm::dvec3& measuredPos){
-    glm::dvec3 filterRes = measuredPos;
-    bool isWrongData = false;
-    if (mCntWrongData < maxCntWrongData){
-        if (std::fabs(measuredPos.x - mLastPos.x) > maxJumpXDist){
-            isWrongData = true;
-            filterRes.x = mLastPos.x;
+    //检查数据是否跳变，跳变数据连续出现五次，则重新初始化滤波器
+    glm::dvec3 filterWrongData(const glm::dvec3& measuredPos) {
+        glm::dvec3 filterRes = measuredPos;
+        bool isWrongData = false;
+        if(mCntWrongData < maxCntWrongData) {  //如果跳变数据小于maxCntWrongData,则继续计数
+            if(std::fabs(measuredPos.x - mLastPos.x) > maxJumpXDist) {
+                isWrongData = true;
+                filterRes.x = mLastPos.x;
+            }
+            if(std::fabs(measuredPos.y - mLastPos.y) > maxJumpYDist) {
+                isWrongData = true;
+                filterRes.y = mLastPos.y;
+            }
+            if(std::fabs(measuredPos.z - mLastPos.z) > maxJumpXDist) {
+                isWrongData = true;
+                filterRes.z = mLastPos.z;
+            }
+        } else {  //跳变数据个数超过maxCntWrongData, 则重新初始化滤波器
+            mInitFlag = false;
+            mCntWrongData = 0;
         }
-        if (std::fabs(measuredPos.y - mLastPos.y) > maxJumpYDist){
-            isWrongData = true;
-            filterRes.y = mLastPos.y;
+
+        if(isWrongData) {  //无跳变数据出现,为正常数据，对目标进行速度预测
+            mCntWrongData = 0;
+        } else {
+            ++mCntWrongData;  //跳变数据出现，进行计数
         }
-        if (std::fabs(measuredPos.z - mLastPos.z) > maxJumpXDist){
-            isWrongData = true;
-            filterRes.z = mLastPos.z;
-        }
-    }else{
-        mInitFlag = false;
-        mCntWrongData = 0;
+        return filterRes;
     }
 
-    if (isWrongData){
-        mCntWrongData = 0;
-    }else{
-        ++mCntWrongData;
-    }
-    return filterRes;
+    void initialKalmanFilter(const glm::dvec3& measuredPos, const TimePoint& curTimePoint) {
+        mX.resize(6);
+        mF.setIdentity(6, 6);
+        mP.setIdentity(6, 6);
+        mQ.setIdentity(6, 6);
+        mH.resize(3, 6);
+        mH << 1.0, 0.0, 0.0,  //
+            0.0, 1.0, 0.0,    //
+            0.0, 0.0, 1.0;    //
+        mR.resize(3, 3);
+        mR << 0.01, 0.0, 0.0,  //
+            0.0, 0.01, 0.0,    //
+            0.0, 0.0, 0.01;
 
-}
+        mLastPos = measuredPos;
+        mLastTimePoint = curTimePoint;
+        setX(measuredPos);
+        mInitFlag = true;
+    }
+
+    void Prediction() {
+        mX = mF * mX;
+        mP = mF * mP * mF.transpose() + mQ;
+    }
+
+    void UpdateMeasurement(const Eigen::VectorXd z) {
+        const auto y = z - mH * mX;  // Measure
+        const auto S = mH * mP * mH.transpose() + mR;
+        const auto K = mP * mH.transpose() * S.inverse();  // Kalman Gain
+        mX = mX + (K * y);                                 // Optimal estimate
+        const auto I = Eigen::MatrixXd::Identity(mX.size(), mX.size());
+        mP = (I - K * mH) * mP;
+    }
+
+    void KmFilter(const glm::dvec3& pos, double dt) {
+        Eigen::MatrixXd inputF(6, 6);
+        inputF << 1.0, 0.0, 0.0, dt, 0.0, 0.0,  //
+            0.0, 1.0, 0.0, 0.0, dt, 0.0,        //
+            0.0, 0.0, 1.0, 0.0, 0.0, dt,        //
+            0.0, 0.0, 0.0, 1.0, 0.0, 0.0,       //
+            0.0, 0.0, 0.0, 0.0, 1.0, 0.0,       //
+            0.0, 0.0, 0.0, 0.0, 0.0, 1.0;       //
+        mF << inputF;
+
+        Prediction();
+        Eigen::VectorXd measuredZ(3, 1);
+        measuredZ << pos.x, pos.y, pos.z;
+        UpdateMeasurement(measuredZ);
+
+        mPredictedVel = { mX(3), mX(4), mX(5) };
+        if(mPredictedVel.x > maxXVel) {  //限幅滤波
+            mPredictedVel.x = maxXVel;
+        }
+        if(mPredictedVel.y > maxYVel) {
+            mPredictedVel.y = maxYVel;
+        }
+        if(mPredictedVel.z > maxZVel) {
+            mPredictedVel.z = maxZVel;
+        }
+    }
+
+    void runFilter(const glm::dvec3& measuredPos, const TimePoint& curTimePoint) {
+        if(!mInitFlag) {
+            initialKalmanFilter(measuredPos, curTimePoint);
+            return;
+        }
+
+        double deltaTime =
+            static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(curTimePoint - mLastTimePoint).count()) /
+            1e6;
+        if(deltaTime > maxDeltaTime) {
+            initialKalmanFilter(measuredPos, curTimePoint);
+            return;
+        }
+        const auto filterPos = filterWrongData(measuredPos);
+        if(!mInitFlag) {
+            initialKalmanFilter(measuredPos, curTimePoint);
+            return;
+        }
+        KmFilter(filterPos, deltaTime);
+    }
+
+public:
+    ArmorPredictor(caf::actor_config& base, const Hubconfig& config) : HubHelper{ base, config }, mKey{ generateKey(this) } {}
+
+    caf::behavior make_behavior() override {
+        return {
+            [](start_atom) { ACTOR_PROTOCOL_CHECK(start_atom); },
+            [this](set_target_atom, Identifier key) {
+                ACTOR_PROTOCOL_CHECK(set_target_atom, TypedIdentifier<SelectedTarget>(SelectedTarget));
+                ACTOR_EXCEPTION_PROBE();
+
+                auto data = BlackBoard::instance().get<SelectedTarget>(key);
+                const auto dataHeadInfo = BlackBoard::instance().get<HeadInfo>(mHeadKey);
+                const auto dataPosture = BlackBoard::instance().get<PostureData>(mIMUKey);
+                if(!(data.has_value() && data.value().selected.has_value() && dataHeadInfo.has_value() &&
+                     dataPosture.has_value()))
+                    return;
+
+                Vector<UnitType::Distance, FrameOfReference::Gun> positionOfReferenceGun(
+                    data.value().selected.value().center.raw());
+                Vector<UnitType::Distance, FrameOfReference::Robot> positionOfReferenceRobot =
+                    dataHeadInfo.value().transform(positionOfReferenceGun);
+                Vector<UnitType::LinearVelocity, FrameOfReference::Ground> linearVelocity(
+                    dataPosture.value().linearVelocityOfRobot.raw());
+                data.value().position = positionOfReferenceRobot;
+
+                if(mConfig.enablePredictor) {  //如果使用预测功能的话，目标相对机器人的速度即为机器人坐标系下，相机所观测的速度
+                    glm::dvec3 measuredPos = positionOfReferenceRobot.raw();
+                    runFilter(measuredPos, data.value().lastUpdate);
+                    data.value().selected.value().velocity.setValue(mPredictedVel);
+                } else {  //如果不使用预测功能的话，将目标看作为静止状态，目标相对机器人的速度即为机器人自身速度取反
+                    data.value().selected.value().velocity.setValue(-linearVelocity.raw());
+                }
+
+                sendMasked(predict_success_atom_v, mGroupMask,
+                           BlackBoard::instance().updateSync<SelectedTarget>(Identifier{ mKey.val }, data));
+            },
+            [this](update_head_atom, GroupMask, Identifier key) {
+                ACTOR_PROTOCOL_CHECK(update_head_atom, GroupMask, TypedIdentifier<HeadInfo>);
+                mHeadKey = key;
+            },
+            [this](update_posture_atom, Identifier key) {
+                ACTOR_PROTOCOL_CHECK(update_posture_atom, TypedIdentifier<PostureData>);
+                mIMUKey = key;
+            }
+
+        };
+    }
+};
--- src/Solve/AngleSolver.cpp
+++ src/Solve/AngleSolver.cpp
@@ -17,81 +17,21 @@
 
 #include "SuppressWarningEnd.hpp"
 
-
 struct AngleSolverSettings final {
     double precision;
     double delay;
-    bool enableEstimateVec;
-    std::vector<double> errEstimate;
-    std::vector<double> errMeasure;
 };
 
 template <class Inspector>
 bool inspect(Inspector& f, AngleSolverSettings& x) {
-    return f.object(x).fields(f.field("precision", x.precision), f.field("delay", x.delay),
-                              f.field("enableEstimateVec", x.enableEstimateVec).fallback(false));
+    return f.object(x).fields(f.field("precision", x.precision), f.field("delay", x.delay));
 }
-
-class EstimateVec final {
-    glm::dvec3 mMeasureVec;
-
-    std::vector<double> mPreTimePoint;
-    std::vector<glm::dvec3> mPrePosition;
-
-    uint64_t cnt;
-    constexpr static size_t estimateStep = 20;
-
-public:
-    EstimateVec() = default;
-
-    EstimateVec(const std::vector<double>& errEstimate, const std::vector<double>& errMeasure)
-        :mMeasureVec(0, 0, 0), cnt(0) {
-        mPreTimePoint.resize(estimateStep);
-        mPrePosition.resize(estimateStep);
-    }
-
-    void update(const TimePoint& curTimePoint, const glm::dvec3& curPosition, const glm::dvec3& linearVelocity) {
-        double tp = static_cast<double>(curTimePoint.time_since_epoch().count()) / 1e9;
-        if(cnt >= 1) {
-            double dt = tp - mPreTimePoint[(cnt - 1) % estimateStep];
-            auto diffPos = curPosition - mPrePosition[(cnt - 1) % estimateStep];
-            logInfo(fmt::format("cnt:{}, dt:{}, diffPos:{}", cnt, dt, glm::length(diffPos)));
-            if(dt > 1.0 || glm::length(diffPos) > 0.5) {  // diff time too long or diff position too distant, re estimate
-                mMeasureVec = { 0, 0, 0 };
-
-                mPreTimePoint.clear();
-                mPreTimePoint.resize(estimateStep);
-
-                mPrePosition.clear();
-                mPrePosition.resize(estimateStep);
-
-                cnt = 0;
-            } else {
-                if(cnt >= estimateStep) {
-                    diffPos = mPrePosition[(cnt - estimateStep) % estimateStep] - curPosition;
-                    dt = tp - mPreTimePoint[(cnt - estimateStep) % estimateStep];
-                    mMeasureVec = diffPos / dt + linearVelocity;
-                    logInfo(fmt::format("mMeasureVec: {}, {}, {}", mMeasureVec.x, mMeasureVec.y, mMeasureVec.z));
-                }
-            }
-        }
-        mPreTimePoint[(cnt) % estimateStep] = tp;
-        mPrePosition[(cnt) % estimateStep] = curPosition;
-        ++cnt;
-    }
-
-    glm::dvec3 getEstimateVec() {
-        return mMeasureVec;
-    }
-};
 
 class AngleSolver final : public HubHelper<caf::event_based_actor, AngleSolverSettings, set_target_info_atom> {
     Identifier mKey, mIMUKey, mHeadKey;
-    EstimateVec estimator;
 
 public:
-    AngleSolver(caf::actor_config& base, const HubConfig& config)
-        : HubHelper{ base, config }, mKey{ generateKey(this) }, estimator(mConfig.errEstimate, mConfig.errMeasure) {}
+    AngleSolver(caf::actor_config& base, const HubConfig& config) : HubHelper{ base, config }, mKey{ generateKey(this) } {}
 
     static std::complex<double> sqrtN(const std::complex<double>& x, double n) {
         if(auto r = std::hypot(x.real(), x.imag()); r > 0.0) {
@@ -171,16 +111,12 @@ public:
         return {
             [this](start_atom) { ACTOR_PROTOCOL_CHECK(start_atom); },
             [this](set_target_atom, Identifier key) {
-                ACTOR_PROTOCOL_CHECK(set_target_atom, TypedIdentifier<SelectedTarget>);
+                ACTOR_PROTOCOL_CHECK(predict_success_atom, TypedIdentifier<SelectedTarget>);
                 ACTOR_EXCEPTION_PROBE();
 
-                const auto data = BlackBoard::instance().get<SelectedTarget>(key);
-                const auto dataHeadInfo = BlackBoard::instance().get<HeadInfo>(mHeadKey);
-                const auto dataPosture = BlackBoard::instance().get<PostureData>(mIMUKey);
-                if(!(data.has_value() && data.value().selected.has_value() && dataHeadInfo.has_value() &&
-                     dataPosture.has_value()))
+                auto data = BlackBoard::instance().get<SelectedTarget>(key);
+                if(!(data.has_value() && data.value().selected.has_value()))
                     return;
-
                 HubLogger::watch("armor type", magic_enum::enum_name(data.value().selected.value().type));
 
                 const auto& globalSettings = GlobalSettings::get();
@@ -190,32 +126,17 @@ public:
 
                 const auto delayTime = mConfig.delay;
 
-                logInfo(fmt::format("Beform transform:x: {}, y: {}, z: {}", data.value().selected.value().center.raw().x,
-                                    -data.value().selected.value().center.raw().z, data.value().selected.value().center.raw().y));
-                Vector<UnitType::Distance, FrameOfReference::Gun> positionOfReferenceGun(
-                    data.value().selected.value().center.raw());
-                Vector<UnitType::Distance, FrameOfReference::Robot> positionOfReferenceRobot =
-                    dataHeadInfo.value().transform(positionOfReferenceGun);
-                Vector<UnitType::LinearVelocity, FrameOfReference::Ground> linearVelocity(
-                    dataPosture.value().linearVelocityOfRobot.raw());
-
+                Vector<UnitType::Distance, FrameOfReference::Robot> positionOfReferenceRobot = data.value().position;
+                Vector<UnitType::LinearVelocity, FrameOfReference::Robot> linearVel = data.value().selected.value().velocity;
                 HubLogger::watch("z", positionOfReferenceRobot.raw().z);
 
                 //(forward:+y,right:+x)
                 glm::dvec3 transformedLinearVelocity = { 0, 0, 0 };
                 auto targetVec = data.value().selected.value().velocity;
-                targetVec.setZero();
 
-                if(mConfig.enableEstimateVec) {
-                    estimator.update(data.value().lastUpdate, positionOfReferenceRobot.raw(), linearVelocity.raw());
-                    targetVec.setValue(estimator.getEstimateVec());
-                    logInfo(fmt::format("target vec:{}, {}, {}", targetVec.raw().x, targetVec.raw().y, targetVec.raw().z));
-                }
                 glm::dvec3 transformedPosition = { positionOfReferenceRobot.raw().x, -positionOfReferenceRobot.raw().z,
                                                    positionOfReferenceRobot.raw().y };
-                transformedLinearVelocity = { -targetVec.raw().x - linearVelocity.raw().x,
-                                              targetVec.raw().z + linearVelocity.raw().z,
-                                              -targetVec.raw().y - linearVelocity.raw().y };
+                transformedLinearVelocity = { -linearVel.raw().x, +linearVel.raw().z, -linearVel.raw().y };
                 // logInfo(fmt::format("Source Velocity {} {} {}", linearVelocity.raw().x, linearVelocity.raw().y,
                 // linearVelocity.raw().z));
                 transformedPosition = { transformedPosition.x + delayTime * transformedLinearVelocity.x,
@@ -244,14 +165,7 @@ public:
                 sendAll(set_target_info_atom_v, mGroupMask, data.value().lastUpdate.time_since_epoch().count(), yawAngle,
                         pitchAngle, isFire);
             },
-            [this](update_head_atom, GroupMask, Identifier key) {
-                ACTOR_PROTOCOL_CHECK(update_head_atom, GroupMask, TypedIdentifier<HeadInfo>);
-                mHeadKey = key;
-            },
-            [this](update_posture_atom, Identifier key) {
-                ACTOR_PROTOCOL_CHECK(update_posture_atom, TypedIdentifier<PostureData>);
-                mIMUKey = key;
-            }
+
         };
     }
 };
