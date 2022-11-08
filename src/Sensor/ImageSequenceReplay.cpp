367489b3128e24470f043252d3442b40483d1e42
梁志辉 <12132276@mail.sustech.edu.cn>
2022-11-09 00:02:34 +0800

    fix(ImageSequenceRepaly):fix no debug info

--- src/Sensor/ImageSequenceReplay.cpp
+++ src/Sensor/ImageSequenceReplay.cpp
@@ -78,6 +78,7 @@ public:
             if(mImages.size() == 0) {
                 const auto errInfo =
                     fmt::format("Path:{} do not exsit picture with extension {}", mConfig.path, mConfig.extension);
+                logErr(errInfo);
                 throw std::invalid_argument::exception();
             }
         }
