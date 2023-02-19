f13576bf12c4262c9d5465f77661fd0258f7eeae
12111224 <12111224@mail.sustech.edu.cn>
2023-02-20 00:08:52 +0800

    fix(fix build error)

--- src/Sensor/DahengDriver.cpp
+++ src/Sensor/DahengDriver.cpp
@@ -153,7 +153,7 @@ class DahengDriver final : public HubHelper<caf::event_based_actor, DahengDriver
 #ifdef ARTINX_RADAR
         sendAll(image_frame_atom_v, BlackBoard::instance().updateSync(mKey, std::move(frameData), std::string_view(mConfig.cameraName)));
 #else
-        sendAll(image_frame_atom_v, BlackBoard::instance().updateSync(mKey, std::move(frameData), std::string("Origin")));
+        sendAll(image_frame_atom_v, BlackBoard::instance().updateSync(mKey, std::move(frameData), std::string_view("Origin")));
 #endif
     }
 
