92f3dbf52b4af48d8a42731dcba0a6fe2dc66c71
oyan <12211831@mail.sustech.edu.cn>
2023-04-21 12:59:21 +0800

    feat(Hero): change hero strategy to select large armor first

--- src/Strategy/Hero.cpp
+++ src/Strategy/Hero.cpp
@@ -60,7 +60,9 @@ public:
 
                 auto minDistance = std::numeric_limits<double>::max();
                 for(auto& target : data.targets) {
-                    if(const auto distance = glm::length(target.center.mVal); minDistance > distance) {
+                    if(const auto distance = glm::length(target.center.mVal);
+                       ((target.type == selected.selected->type) && (minDistance > distance)) ||
+                       ((target.type == ArmorType::Large) && (selected.selected->type == ArmorType::Small))) {
                         selected.selected = target;
                         minDistance = distance;
                     }
