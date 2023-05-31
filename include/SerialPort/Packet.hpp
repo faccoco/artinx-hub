f8125bcf0837c94fbf357bd3e1d0eee0c75bbda3
lzh <1518705978@qq.com>
2023-05-31 15:50:08 +0800

    refactor(packet.hpp):remove packet define in packet.hpp
    Former-commit-id: 0f5abd4135563fb1c781b06e94c219a764c83e89

--- include/SerialPort/Packet.hpp
+++ include/SerialPort/Packet.hpp
@@ -10,65 +10,6 @@
 #include <cstring>
 #include <iterator>
 
-struct FdbPacket final {
-    static constexpr uint16_t id = 0x0A;
-    float yaw, pitch, downYaw, downPitch, bulletSpeed, speedX, speedY;
-    uint8_t color, shooterId, energyMode, outpostMode;
-    float capEnergy, chasisPower;
-    uint16_t shootDelayTime;  // ms
-    explicit FdbPacket(std::array<uint8_t, 1024>& buffer) {
-        PacketReader<1024> reader(buffer);
-        yaw = reader.readCompressedFloat(-4.0f, 0.0005f);
-        pitch = reader.readCompressedFloat(-4.0f, 0.0005f);
-        downYaw = reader.readCompressedFloat(-4.0f, 0.0005f);
-        downPitch = reader.readCompressedFloat(-4.0f, 0.0005f);
-        speedX = reader.readCompressedFloat(-20.0f, 0.01f);
-        speedY = reader.readCompressedFloat(-20.0f, 0.01f);
-        const auto mask = reader.read();
-        color = mask & 1;
-        shooterId = (mask >> 1) & 1;
-        energyMode = (mask >> 2) & 1;
-        outpostMode = (mask >> 4) & 1;
-        bulletSpeed = reader.readCompressedFloat(-1.0f, 0.005f);
-        capEnergy = reader.readCompressedFloat(-1.0f, 0.1f);
-        chasisPower = reader.readCompressedFloat(-1.0f, 0.01f);
-        shootDelayTime = reader.read<uint16_t>();
-    }
-};
-
-class GimbalSetPacket final {
-    static constexpr uint16_t id = 0x0F;
-
-    struct Info {
-        float yaw, pitch;
-        bool isFire;
-    };
-
-    Info up{};
-    uint8_t hasTargets{};
-    PacketBuffer<5, id> buffer{};
-
-    void setUpTarget(float yaw, float pitch, bool isFire) {
-        up = { yaw, pitch, isFire };
-    }
-
-    void setHasTargetBits(uint8_t targetBits) {
-        hasTargets = targetBits;
-    }
-
-    void setPacketData(float yaw, float pitch, uint8_t targetBits, bool isFire) {
-        up = { yaw, pitch, isFire };
-        hasTargets = targetBits;
-    }
-    void serialize() {
-        buffer = {};
-        buffer.serialize(up.yaw, -4.0f, 0.0005f);
-        buffer.serialize(up.pitch, -4.0f, 0.0005f);
-        buffer.serialize(static_cast<uint8_t>(static_cast<uint8_t>(up.isFire) | (hasTargets << 2)));
-        buffer.serializeCrc16();
-    }
-};
-
 struct MapData final {
     uint16_t botID;
     float x;
