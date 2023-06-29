7fc19cfdf344de43fd270df09e9430996ed20622
old_sentry <12011404@mail.sustech.edu.cn>
2023-06-29 23:47:06 +0800

    fix(serialPort): fix no add to cmake
    Former-commit-id: 7184c50aebf6b476f9563311b30e1dfcbd90e5da

--- bringup_templates/bringup.bash
+++ bringup_templates/bringup.bash
@@ -10,6 +10,6 @@ cd "$path" || exit # if fails to cd, exits.
 cd ..
 #ArtinxHub
 while [ true ]; do
-  /opt/artinx-hub/build/src/ArtinxHub /opt/artinx-hub/deploy_config/$(cat /opt/deploy_target.conf).conf 2>/opt/artinx-hub.log
+  /home/artinx-7/Desktop/workspaces/codes/artinx-hub/cmake-build-release/src/ArtinxHub /home/artinx-7/Desktop/workspaces/codes/artinx-hub/deploy_config/radar_recorder.conf 2>/home/artinx-7/Desktop/workspaces/codes/artinx-hub/radar.log
   sleep 1
 done
