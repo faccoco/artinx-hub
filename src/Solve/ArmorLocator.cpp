784586930dd86779dc01d8382fd67dd6258b1585
Yingwei Zheng <12012617@mail.sustech.edu.cn>
2023-07-03 22:05:11 +0800

    fix(InfantrySerialPort.cpp): fix serial port
    Former-commit-id: c9ae88bb4d380cc4dd9c5608a22df6691a475740

--- src/Solve/ArmorLocator.cpp
+++ src/Solve/ArmorLocator.cpp
@@ -83,9 +83,9 @@ public:
                     auto axis = rvecRefCam.mVal / angle;
                     rmat = glm::mat4_cast(glm::angleAxis(-angle, axis));
 
-                    HubLogger::watch("XRefCam", rvecRefCam.mVal.x);
-                    HubLogger::watch("YRefCam", rvecRefCam.mVal.y);
-                    HubLogger::watch("ZRefCam", rvecRefCam.mVal.z);
+                    HubLogger::watch("XRefCam", pointRefGun.mVal.x);
+                    HubLogger::watch("YRefCam", pointRefGun.mVal.y);
+                    HubLogger::watch("ZRefCam", pointRefGun.mVal.z);
                     HubLogger::watch("isLargeArmor", armor.isLargeArmor);
                     // logInfo(fmt::format("isLargeArmor: {} {}", armor.ratio, isLargeArmor));
                     // HubLogger::watch("YawRefCam", glm::degrees(-atan2(rmat.raw()[2][0], rmat.raw()[2][2])));
