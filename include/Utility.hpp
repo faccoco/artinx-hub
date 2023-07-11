e13697343bfa68d2ffa081e2ea4749f0b2c13c90
ljr <12210105@mail.sustech.edu.cn>
2023-07-11 22:35:42 +0800

    fix!(MathTools, AngleSolver): Fix returning nonreal solns; Provide accessibility in return values; ferrari() now returns optional value; solveWithoutAirDrag() now added a bool in its returning tuple, indicating the accesibility;
    Former-commit-id: 45ebe8b743f8ac6b1bf03400c97d4f8febb7bd32

--- include/Utility.hpp
+++ include/Utility.hpp
@@ -100,11 +100,11 @@ typename Seq::value_type Std(const Seq& array, T avg) {
 
 std::complex<double> sqrtN(const std::complex<double>& x, double n);
 
-double ferrari(std::complex<double> a, std::complex<double> b, std::complex<double> c, std::complex<double> d,
+std::optional<double> ferrari(std::complex<double> a, std::complex<double> b, std::complex<double> c, std::complex<double> d,
                std::complex<double> e);
 
 // tuple[time,yawAngle,pitchAngle]
-std::tuple<double, double, double> solveWithoutAirDrag(glm::dvec3 targetPos, glm::dvec3 targetVel);
+std::tuple<bool, double, double, double> solveWithoutAirDrag(glm::dvec3 targetPos, glm::dvec3 targetVel);
 
 // -pi < res <= pi
 [[nodiscard]] double normalizeAngle(double a);
