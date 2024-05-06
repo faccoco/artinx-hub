1326be4045daff5345d211684b2fc7db4ecb7af8
Zeng Zishan <12310733@mail.sustech.edu.cn>
2024-05-06 12:05:25 +0800

    fix(src/Solve/AngleSolver.cpp): fix the lack of the init of orietationAngle
    fix hasTarget = 0 because of the lack of the init of the parameter
    orietationAngle
    
     37 degrees is calculated by arcsin(600/(600+400)), the
    largest but impossible orietationAngle

--- src/Solve/AngleSolver.cpp
+++ src/Solve/AngleSolver.cpp
@@ -32,7 +32,8 @@ bool inspect(Inspector& f, AngleSolverSettings& x) {
     return f.object(x).fields(
         f.field("delay", x.delay).fallback(0.0), f.field("sameTimeThreshold", x.sameTimeThreshold).fallback(0.05),
         f.field("requiredTimeWeight", x.requiredTimeWeight).fallback(1),
-        f.field("maxShootDeltaTheta", x.maxShootDeltaTheta).fallback(60), f.field("lVelDiscount", x.lVelDiscount).fallback(1.0));
+        f.field("maxShootDeltaTheta", x.maxShootDeltaTheta).fallback(60), f.field("lVelDiscount", x.lVelDiscount).fallback(1.0),
+        f.field("orietationAngle", x.orietationAngle).fallback(37));
 }
 
 class AngleSolver final : public HubHelper<caf::event_based_actor, AngleSolverSettings, set_target_info_atom> {
