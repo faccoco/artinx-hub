7382bb14b1a8bf63cfe41bc1d2f8bdffcb964f8d
12012710 <12012710@mail.sustech.edu.cn>
2024-06-26 13:59:01 +0800

    feat(RosCamera): add ros camera communication

--- src/Utility/RosCamera.cpp
+++ src/Utility/RosCamera.cpp
@@ -2,8 +2,6 @@
 // Created by 12012710 on 24-6-23.
 //
 
-#include <cv_bridge/cv_bridge.hpp>
-#include <cv_bridge/cv_bridge_export.h>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
 #include <sensor_msgs/msg/image.hpp>
@@ -11,11 +9,13 @@
 #include <caf/event_based_actor.hpp>
 
 #include "BlackBoard.hpp"
+#include "CameraFrame.hpp"
+#include "ExceptionProbe.hpp"
 #include "Hub.hpp"
 
-#include "CameraBase.hpp"
-#include <CameraFrame.hpp>
-#include <ExceptionProbe.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 
 struct RosCameraSettings final {
     std::string ip;
@@ -24,16 +24,13 @@ struct RosCameraSettings final {
 };
 
 template <class Inspector>
-bool inspect(Inspector& f, Ros& x) {
-    return f.object(x).fields(
-        f.field("ip", x.ip).fallback("127.0.0.1"),
-        f.field("imageTopic", x.imageTopic).fallback("/image"),
-        f.field("cameraInfoTopic", x.cameraInfoTopic).fallback("/camera_info"));
+bool inspect(Inspector& f, RosCameraSettings& x) {
+    return f.object(x).fields(f.field("ip", x.ip).fallback("127.0.0.1"), f.field("imageTopic", x.imageTopic).fallback("/image"),
+                              f.field("cameraInfoTopic", x.cameraInfoTopic).fallback("/camera_info"));
 }
 //
-class RosCamera final
-    : public HubHelper<caf::event_based_actor, CameraBaseSettings, image_frame_atom> {
-    Identifier mKey{};
+class RosCamera final : public HubHelper<caf::event_based_actor, RosCameraSettings, image_frame_atom> {
+    Identifier mKey;
     rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr cameraInfoSubscription;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr imgSubscription;
     rclcpp::Node::SharedPtr node;
@@ -44,29 +41,29 @@ class RosCamera final
 
 public:
     RosCamera(caf::actor_config& base, const HubConfig& config, std::string name)
-         : HubHelper{ base, config, std::move(name) }, mKey{ generateKey(this) } {
+        : HubHelper{ base, config, std::move(name) }, mKey{ generateKey(this) } {
+        rclcpp::init(0, nullptr);  // init ROS
         // initialize node
-        // node = rclcpp::Node::make_shared("ros_connector");
-        // cameraInfoSubscription = node->create_subscription<sensor_msgs::msg::CameraInfo>(
-        //     "/camera_info", rclcpp::SensorDataQoS(), [this](sensor_msgs::msg::CameraInfo::ConstSharedPtr cameraInfoMsg) {
-        //         camCenter = cv::Point2f(cameraInfoMsg->k[2], cameraInfoMsg->k[5]);
-        //         camerainfo = std::make_shared<sensor_msgs::msg::CameraInfo>(*cameraInfoMsg);
-        //         mCameraMatrix = cv::Mat(3, 3, CV_64F, const_cast<double*>(camerainfo->k.data())).clone();
-        //         mDistCoefficients = cv::Mat(1, 5, CV_64F, const_cast<double*>(camerainfo->d.data())).clone();
-        //         cameraInfoSubscription.reset();
-        //     });
-        // imgSubscription = node->create_subscription<sensor_msgs::msg::Image>(
-        //     "/image", rclcpp::SensorDataQoS(), std::bind(&RosCameraDriver::publisRosImg, this, std::placeholders::_1));
-        //
-        // rclcpp::init(0, nullptr);  // init ROS
-        // std::thread([this]() { rclcpp::spin(node); }).detach();
+        node = rclcpp::Node::make_shared("ros_connector");
+        cameraInfoSubscription = node->create_subscription<sensor_msgs::msg::CameraInfo>(
+            mConfig.cameraInfoTopic, rclcpp::SensorDataQoS(), [this](sensor_msgs::msg::CameraInfo::ConstSharedPtr cameraInfoMsg) {
+                camCenter = cv::Point2f(cameraInfoMsg->k[2], cameraInfoMsg->k[5]);
+                camerainfo = std::make_shared<sensor_msgs::msg::CameraInfo>(*cameraInfoMsg);
+                mCameraMatrix = cv::Mat(3, 3, CV_64F, const_cast<double*>(camerainfo->k.data())).clone();
+                mDistCoefficients = cv::Mat(1, 5, CV_64F, const_cast<double*>(camerainfo->d.data())).clone();
+                cameraInfoSubscription.reset();
+            });
+        imgSubscription = node->create_subscription<sensor_msgs::msg::Image>(
+            mConfig.imageTopic, rclcpp::SensorDataQoS(), std::bind(&RosCamera::publishRosImg, this, std::placeholders::_1));
+        std::thread([this]() { rclcpp::spin(node); }).detach();
     }
 
-    void publisRosImg(const sensor_msgs::msg::Image::ConstSharedPtr& imgMsg) {
+    void publishRosImg(const sensor_msgs::msg::Image::ConstSharedPtr& imgMsg) {
         ACTOR_EXCEPTION_PROBE();
         // const auto frame = cv_bridge::toCvShare(imgMsg, "rgb8")->image;
         // const auto frame = cv_bridge::toCvShare(imgMsg, "rgb8")->image;
-        cv::Mat frame;
+        // cv::Mat frame;
+        const auto frame = convertToCvMat(imgMsg);
         CameraFrame frameData;
         frameData.lastUpdate = SynchronizedClock::instance().now();
         frameData.info.cameraMatrix = mCameraMatrix;
@@ -74,20 +71,58 @@ public:
         frameData.info.identifier = "RosCamera";
         frameData.info.width = frame.cols;
         frameData.info.height = frame.rows;
-        frameData.info.tfRobot2Camera = clcTfRobot2Camera(Pose{ 0.0, 0.0, 0.0 });
+        frameData.info.tfRobot2Camera = clcTfRobot2Camera(0.0, 0.0, 0.0);
         frame.copyTo(frameData.frame);
 
         sendAll(image_frame_atom_v,
-                BlackBoard::instance().updateSync(mKey, std::move(frameData), static_cast<std::string_view>(mConfig.cameraName)));
+                BlackBoard::instance().updateSync(mKey, std::move(frameData), static_cast<std::string_view>("ros_camera")));
     }
 
-    Transform<FrameOfRef::Robot, FrameOfRef::Camera, true> clcTfRobot2Camera(const Pose& gunPose) {
-        const double yaw = gunPose.yaw;
-        const double pitch = mConfig.isAtGun ? gunPose.pitch : 0;
-        const double roll = gunPose.roll;
+    Transform<FrameOfRef::Robot, FrameOfRef::Camera, true> clcTfRobot2Camera(const double yaw, const double pitch,
+                                                                             const double roll) {
         return glm::rotate(
-                   glm::rotate(glm::rotate(glm::identity<glm::dmat4>(), -roll, glm::dvec3{ 0, 0, 1 }), -pitch, glm::dvec3{ 1, 0, 0 }),
-                   -yaw, glm::dvec3{ 0, 1, 0 });
+            glm::rotate(glm::rotate(glm::identity<glm::dmat4>(), -roll, glm::dvec3{ 0, 0, 1 }), -pitch, glm::dvec3{ 1, 0, 0 }),
+            -yaw, glm::dvec3{ 0, 1, 0 });
+    }
+
+    caf::behavior make_behavior() override {
+        return { [this](start_atom) { ACTOR_PROTOCOL_CHECK(start_atom); } };
+    };
+
+    static cv::Mat convertToCvMat(const sensor_msgs::msg::Image::ConstSharedPtr msg)
+    {
+        // Convert the ROS Image message to OpenCV image
+        int cv_type;
+        if (msg->encoding == "mono8")
+        {
+            cv_type = CV_8UC1;
+        }
+        else if (msg->encoding == "bgr8" || msg->encoding == "rgb8")
+        {
+            cv_type = CV_8UC3;
+        }
+        else if (msg->encoding == "mono16")
+        {
+            cv_type = CV_16UC1;
+        }
+        else if (msg->encoding == "rgba8")
+        {
+            cv_type = CV_8UC4;
+        }
+        else
+        {
+            logInfo(fmt::format("Unsupported encoding type: {}", msg->encoding));
+            return {};
+        }
+
+        cv::Mat mat(msg->height, msg->width, cv_type, const_cast<unsigned char*>(msg->data.data()), msg->step);
+
+        if (msg->encoding == "rgb8")
+        {
+            cv::cvtColor(mat, mat, cv::COLOR_RGB2BGR);
+        }
+        return mat.clone(); // Ensure data is copied
     }
 };
+
 HUB_REGISTER_CLASS(RosCamera);
